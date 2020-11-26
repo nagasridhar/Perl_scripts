a9ae9e7cc1381890e77dd3501b435d940ebe5eb5
nagasridhar <30839649+nagasridhar@users.noreply.github.com>
2020-11-26 15:11:36 -0500

    Updated to split each file

--- split_fasta.pl
+++ split_fasta.pl
@@ -7,7 +7,9 @@ my @header;
 my @sequence;
 my $n=0;
 my $split;
-
+my $increment=0;
+my $output_file;
+my $initial=0;
 open (FILE,$filename_fasta) or die "No input file provided or the input file does not exist in the path, '$filename_fasta' $!";
 while (my $file = <FILE>){
 chomp $file;
@@ -32,15 +34,38 @@ $sequence[$n] .= $file;
 
 
 my $header = scalar(@header);
-print "No. of headers = $header\n";
+#print "No. of headers = $header\n";
 
 if ($header%2 ==0 ){
     $split = ($header/4);
         }
-        
+
     else{
         my $header_count = $header+1;
         $split = ($header_count/4);
     }    
 
-print "No. of genes in each output $split\n";
+    $increment = $split;
+#print "No. of genes in each output $split\n";
+
+# open(my $fh, '>', $output_file) or die "Could not open file '$output_file' $!";
+# foreach(@header){
+# 	my @name=split(/_/,$filename_fasta);
+# 	$name[2]=~ s/.fna.bed//g;
+# 	#print $fh "$name[1]\n";
+# 	print $fh "$name[2]\t$_\n";
+# 	#print "$_\n";
+# }
+
+while($increment<=scalar(@header)){
+$output_file = "output.$increment.fa";
+open(my $fh, '>', $output_file) or die "Could not open file '$output_file' $!";
+
+for ($initial=$initial; $initial<=$increment;$initial++){
+    print $fh "$header[$initial]\n";
+    print $fh "$sequence[$initial]\n";
+}
+$initial=$increment;
+$increment = $increment+$split;
+print "New value of split: $increment\n";
+}
